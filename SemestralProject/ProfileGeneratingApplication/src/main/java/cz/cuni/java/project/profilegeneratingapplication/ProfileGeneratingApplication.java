ecf998c675a9cf7ed68a7825a5e56363ad04ebfe
Adam Lány <adam.lany@posam.sk>
2022-06-11 22:57:50 +0200

    init

---
+++ SemestralProject/ProfileGeneratingApplication/src/main/java/cz/cuni/java/project/profilegeneratingapplication/ProfileGeneratingApplication.java
@@ -0,0 +1,13 @@
+package cz.cuni.java.project.profilegeneratingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileGeneratingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileGeneratingApplication.class, args);
+    }
+
+}
---
+++ SemestralProject/ProfileGeneratingApplication/src/test/java/cz/cuni/java/project/profilegeneratingapplication/ProfileGeneratingApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cuni.java.project.profilegeneratingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileGeneratingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
